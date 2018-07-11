d2aace5c63d792f459ceff591a5f9a6a4762fd98
casa <casa@96c2d22d-f105-cb40-99ec-877b05c57555>
2018-07-11 00:06:40 +0000

    agrego lo del bakcup
    git-svn-id: file:///C/Users/yo/Documents/repositorio_svn_ondontopc/trunk@9 96c2d22d-f105-cb40-99ec-877b05c57555

--- apps/web/modules/compra/actions/actions.class.php
+++ apps/web/modules/compra/actions/actions.class.php
@@ -41,7 +41,7 @@ class compraActions extends autoCompraActions
       $pid = $this->getUser()->getAttribute('pid');
     }
     $proveedor = Doctrine::getTable('Proveedor')->find($pid);
-    $nro = $proveedor->getProxRemito();
+    $nro = $proveedor->getProxRemito() + 1;
     return $this->renderText(json_encode($nro));
   }
 }
--- apps/web/modules/lista/templates/_imprimir.php
+++ apps/web/modules/lista/templates/_imprimir.php
@@ -1,5 +1,65 @@
-<html>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xml:lang="en" xmlns="http://www.w3.org/1999/xhtml" lang="en">
 <head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+
+<title>Listado de Productos</title>
+<style type="text/css">
+@page {
+	margin: 2cm;
+}
+
+body {
+  font-family: sans-serif;
+	margin: 0.5cm 0;
+	text-align: justify;
+}
+
+#header,
+#footer {
+  position: fixed;
+  left: 0;
+	right: 0;
+	color: #666;
+	font-size: 0.9em;
+}
+
+#header {
+  top: 0;
+	border-bottom: 0.1pt solid #666;
+}
+
+#footer {
+  bottom: 0;
+  border-top: 0.1pt solid #666;
+}
+
+#header table,
+#footer table {
+	width: 100%;
+	border-collapse: collapse;
+	border: none;
+}
+
+#header td,
+#footer td {
+  padding: 0;
+	width: 50%;
+}
+
+.page-number {
+  text-align: right;
+}
+
+.page-number:before {
+  content: " " counter(page);
+}
+
+hr {
+  page-break-after: always;
+  border: 0;
+}
+</style>
 </head>
 <body>
 <div id="header">
