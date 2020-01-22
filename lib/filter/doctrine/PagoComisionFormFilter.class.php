0fd5011947b7f3194c3fb12537b9dd780ca0ad92
yo <yo@96c2d22d-f105-cb40-99ec-877b05c57555>
2020-01-22 14:50:39 +0000

    git-svn-id: file:///C/Users/yo/Documents/repositorio_svn_ondontopc/trunk@153 96c2d22d-f105-cb40-99ec-877b05c57555

--- lib/filter/doctrine/PagoComisionFormFilter.class.php
+++ lib/filter/doctrine/PagoComisionFormFilter.class.php
@@ -12,5 +12,10 @@ class PagoComisionFormFilter extends BasePagoComisionFormFilter
 {
   public function configure()
   {
+    $this->widgetSchema['revendedor_id'] = new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('Revendedor'), 'table_method' => 'getActivos', 'add_empty' => true, 'order_by' => array('apellido', 'asc')), array('data-placeholder' => 'Escriba un Nombre...', 'class' => 'chzn-select', 'style' => 'width:350px;'));
+    $this->validatorSchema['revendedor_id'] = new sfValidatorDoctrineChoice(array('required' => false, 'model' => $this->getRelatedModelName('Revendedor')));
+    
+		$this->widgetSchema['fecha'] = new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDateJQueryUI(array("change_month" => true, "change_year" => true)), 'to_date' => new sfWidgetFormDateJQueryUI(array("change_month" => true, "change_year" => true)), 'with_empty' => false, 'template' => 'desde %from_date%<br />hasta %to_date%'));		
+    $this->validatorSchema['fecha'] = new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDate(array('required' => false, 'date_format' => '~(?P<day>\d{2})/(?P<month>\d{2})/(?P<year>\d{4})~')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'date_format' => '~(?P<day>\d{2})/(?P<month>\d{2})/(?P<year>\d{4})~'))));
   }
 }
