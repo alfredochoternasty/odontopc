a881fa5d6c61e6e079ccf5c3a83d572c9776d969
yo <yo@96c2d22d-f105-cb40-99ec-877b05c57555>
2020-07-19 21:39:17 +0000

    git-svn-id: file:///C/Users/yo/Documents/repositorio_svn_ondontopc/trunk@227 96c2d22d-f105-cb40-99ec-877b05c57555

--- apps/web/modules/sfGuardAuth/templates/_signin_form.php
+++ apps/web/modules/sfGuardAuth/templates/_signin_form.php
@@ -1,18 +1,9 @@
 <?php use_stylesheet('login.css') ?>
 <?php use_stylesheet('reset.css') ?>
-
-<form class="box login" action="<?php echo url_for('@sf_guard_signin') ?>" method="post">
-	<fieldset class="boxBody">
-	  <?php 
-    echo $form['username']->renderError();
-    echo $form['username']->renderLabel();
-    echo $form['username']->render();
-	  echo $form['password']->renderLabel();
-	  echo $form['password']->render();
-    echo $form['_csrf_token']; 
-    ?>
-	</fieldset>
-	<footer>
-	  <input type="submit" class="btnLogin" value="Iniciar Sesión" tabindex="3">
-	</footer>
+<form class="form-login" action="<?php echo url_for('@sf_guard_signin') ?>" method="post">
+	<div class="msg_error"><?php echo $form['username']->renderError() ?></div>
+	<input class="form-componente user_icon" type="text" name="signin[username]" placeholder="Usuario">
+	<input class="form-componente pass_icon" type="password" name="signin[password]" placeholder="Clave">
+  <?php echo $form['_csrf_token']; ?>
+	<button class="boton-login" type="submit">Ingresar</button>
 </form>
--- apps/web/modules/sfGuardAuth/templates/signinSuccess.php
+++ apps/web/modules/sfGuardAuth/templates/signinSuccess.php
@@ -1,1 +1,5 @@
+<?php use_stylesheet('login.css') ?>
+<?php use_stylesheet('reset.css') ?>
+<?php $base_url = $sf_user->getVarConfig('base_url'); ?>
+<div class="cabecera"><img src="<?php echo $base_url ?>/web/images/logo_nti.png"></div>
 <?php echo get_partial('sfGuardAuth/signin_form', array('form' => $form)) ?>
