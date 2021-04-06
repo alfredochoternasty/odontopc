84e2cba3dd8bced7d2885f4b1cb2a95c3cddb44c
yo <yo@96c2d22d-f105-cb40-99ec-877b05c57555>
2021-04-06 00:18:59 +0000

    git-svn-id: file:///C/Users/yo/Documents/repositorio_svn_ondontopc/trunk@361 96c2d22d-f105-cb40-99ec-877b05c57555

--- lib/form/doctrine/PresupuestoForm.class.php
+++ lib/form/doctrine/PresupuestoForm.class.php
@@ -17,7 +17,7 @@ class PresupuestoForm extends BasePresupuestoForm
 		unset($this['vendido'], $this['fecha_venta'], $this['usuario_id']);
 		
     $this->widgetSchema['cliente_id'] = new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('Cliente'), 'table_method' => 'getActivosVta', 'method' => 'getDescAfip', 'add_empty' => true, 'order_by' => array('apellido', 'asc')), array('data-placeholder' => 'Escriba un Nombre...', 'class' => 'chzn-select', 'style' => 'width:350px;'));
-    $this->validatorSchema['cliente_id'] = new sfValidatorDoctrineChoice(array('model' => $this->getRelatedModelName('Cliente'))); 
+    $this->validatorSchema['cliente_id'] = new sfValidatorDoctrineChoice(array('model' => $this->getRelatedModelName('Cliente'), 'required' => false));
 		
     $this->widgetSchema['fecha'] = new sfWidgetFormDateJQueryUI(array("change_month" => true, "change_year" => true));
     $this->validatorSchema['fecha'] = new sfValidatorDate(array('date_format' => '~(?P<day>\d{2})/(?P<month>\d{2})/(?P<year>\d{4})~'));
