417ab0c69ea1551321b4a7e06b96844b5575b313
yo <yo@96c2d22d-f105-cb40-99ec-877b05c57555>
2021-07-08 05:43:23 +0000

    git-svn-id: file:///C/Users/yo/Documents/repositorio_svn_ondontopc/trunk@374 96c2d22d-f105-cb40-99ec-877b05c57555

--- data/sql/nuevo.sql
+++ data/sql/nuevo.sql
@@ -1,8 +1,19 @@
-UPDATE sf_guard_permission SET name = 'Tipos de Ventas', description = '@tipo_venta' WHERE (id = '1430');
-
-ALTER TABLE tipo_venta ADD COLUMN porc_recargo INT(11) NULL AFTER nombre;
-
-INSERT INTO tipo_venta (nombre) VALUES ('Tarj. Cred. 1 Cuota');
-INSERT INTO tipo_venta (nombre) VALUES ('Tarj. Cred. 3 Cuota');
-INSERT INTO tipo_venta (nombre) VALUES ('Tarj. Cred. 6 Cuota');
-INSERT INTO tipo_venta (nombre) VALUES ('Tarj. Cred. 12 Cuota');
+DROP VIEW cliente_ultima_compra;
+CREATE VIEW cliente_ultima_compra AS select 
+	id, 
+	zona_id,
+	apellido, 
+	nombre,
+	telefono, 
+	email, 
+	celular,
+	(select max(r1.fecha) from resumen r1 where r1.cliente_id = c.id) as fecha,
+  (
+    select max(r2.id) 
+    from resumen r2
+    where r2.cliente_id = c.id 
+      and r2.fecha = (
+        select max(r3.fecha) from resumen r3 where r3.cliente_id = c.id
+      )
+  ) as resumen_id
+from cliente c;
