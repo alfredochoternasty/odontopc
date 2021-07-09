840b18baa08f3ca867a47c07a22a19306305759e
yo <yo@96c2d22d-f105-cb40-99ec-877b05c57555>
2021-07-09 17:19:33 +0000

    git-svn-id: file:///C/Users/yo/Documents/repositorio_svn_ondontopc/trunk@375 96c2d22d-f105-cb40-99ec-877b05c57555

--- plugins/acDompdfPlugin/lib/vendor/dompdf/include/autoload.inc.php
+++ plugins/acDompdfPlugin/lib/vendor/dompdf/include/autoload.inc.php
@@ -80,7 +80,8 @@ else if ( !function_exists("__autoload") ) {
    *
    * @param string $class
    */
-  function __autoload($class) {
-    DOMPDF_autoload($class);
-  }
+  // function __autoload($class) {
+    // DOMPDF_autoload($class);
+  // }
+  spl_autoload_register($class);
 }
